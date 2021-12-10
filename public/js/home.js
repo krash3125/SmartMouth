c51e75f7d1eb80592badba3e77a7f4e48a07cd1f
krash3125 <57548935+krash3125@users.noreply.github.com>
2021-12-10 00:45:50 -0800

    Full Redo
    restructured everything
    fixed server
    added custom font
    change design
    added point system

--- public/js/home.js
+++ public/js/home.js
@@ -1,13 +1,18 @@
 
+function newGame() {
+    window.location.href = "/game/" + (Math.floor(1000 + Math.random() * 9000));
+}
 
-function join() {
-    if(document.getElementById("name").value!=""){
-        sessionStorage.setItem("name", document.getElementById("name").value);
-        window.location.href = "/game/" + (Math.floor(1000 + Math.random() * 9000));
-
+function joinGame() {
+    //needs to check if room exists
+    let code = $("#codeInput").val()
+    if(code.length==4){
+        window.location.href = "/game/" + code
     } 
 }
 
+
 window.onload = () => {
-    document.getElementById('join').addEventListener('click', ()=> join() )
+    $('#newGame').click(()=> newGame())
+    $('#joinGame').click(()=> joinGame())
 }
