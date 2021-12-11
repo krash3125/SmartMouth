e5067af8420237083db2ad834264bd479d8ed5b2
krash3125 <khannakushagr@gmail.com>
2021-12-10 17:04:15 -0800

    Vote Skip + Fixed Filter But
    Added Vote Skip + Fixed Filter Bug
    
    Need to fix when player dc bug - not that apparent
    need to add google analytics
    need to add https
    need to make it so player list is on the side rather than above input

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname)));
 
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
- 
+
 //page routing
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'docs', 'home.html'))
@@ -41,6 +41,7 @@ class User {
     this.name = name;
     this.points = points;
     this.roomCode = roomCode;
+    this.voteSkip = false;
   }
 
   addPoints(p) {
@@ -55,27 +56,27 @@ const serverRooms = new Map(); //a map of room codes
 const checkWord = require('check-word')
 const dictionary = checkWord('en')
 let gameStarted = false
-const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const firstLetters = "wmplctsthpmbsdlngkakenogoerywacdyrbh"
+const secondLetters = "ypmgmsewodetrbonpnlkgkawdbsyrhtacclh"
 
 
 io.on('connection', (socket) => {
-  const userInit = (name) => { 
+  const userInit = (name) => {
     const newUser = new User(socket.id, name, 0, serverCode)
     if (serverRooms.has(serverCode)) { // rand room code if no code exists
-      serverRooms.get(serverCode).push( newUser )
+      serverRooms.get(serverCode).push(newUser)
     }
     else {
-      serverRooms.set(serverCode, [ newUser ] )
+      serverRooms.set(serverCode, [newUser])
     }
     socketIdMap.set(socket.id, newUser)
     socket.join(serverCode)
 
     io.to(serverCode).emit('updatePlayerList', serverRooms.get(serverCode))
   }
-  
+
   const newLetters = () => {
-    let alphabet = "abcdefghijklmnopqrstuvwxyz"
-    let letters = [alphabet[Math.floor(Math.random() * alphabet.length)], alphabet[Math.floor(Math.random() * alphabet.length)]]
+    let letters = [firstLetters[Math.floor(Math.random() * firstLetters.length)], secondLetters[Math.floor(Math.random() * secondLetters.length)]]
     io.to(serverCode).emit('letterChange', letters)
   }
 
@@ -84,27 +85,50 @@ io.on('connection', (socket) => {
   })
 
   socket.on('disconnect', () => {
-    serverRooms.set(serverCode, serverRooms.get(serverCode).filter(user => user.socketId != socket.id))
-    socketIdMap.delete(socket.id)
-    // console.log(serverRooms)
-    // console.log(socketIdMap)
-    //close room if 0 players in game
-    io.to(serverCode).emit('updatePlayerList', serverRooms.get(serverCode))
+    if (serverRooms.get(serverCode) != null) {
+
+      serverRooms.set(serverCode, serverRooms.get(serverCode).filter(user => user.socketId != socket.id))
+      socketIdMap.delete(socket.id)
+      // console.log(serverRooms)
+      // console.log(socketIdMap)
+      //close room if 0 players in game
+      if (serverRooms.get(serverCode).length == 0) {
+        serverRooms.delete(serverCode)
+      }
+      io.to(serverCode).emit('updatePlayerList', serverRooms.get(serverCode))
+    }
   })
 
   socket.on('startGame', () => {
     io.to(serverCode).emit('startGameClients')
-    setTimeout( () => newLetters() , 1000);
+    setTimeout(() => newLetters(), 1000);
   })
 
   socket.on('submitGuess', (word) => {
     if (dictionary.check(word)) {
       socketIdMap.get(socket.id).addPoints(100)
-      io.to(serverCode).emit('updatePlayerList', serverRooms.get(serverCode))
-      io.to(serverCode).emit('clearInput')
-      io.to(serverCode).emit('lastWord', word)
+      io.to(serverCode).emit('playerCorrectGuess', serverRooms.get(serverCode), word)
       newLetters()
+    }
+  })
 
+  socket.on('voteSkip', () => {
+    socketIdMap.get(socket.id).voteSkip = !socketIdMap.get(socket.id).voteSkip
+    let voteSkipCount = 0
+    serverRooms.get(serverCode).forEach((user) => {
+      if (user.voteSkip == true) {
+        voteSkipCount++
+      }
+    })
+    if (voteSkipCount / serverRooms.get(serverCode).length > .6) {
+      serverRooms.get(serverCode).forEach((user) => {
+        user.voteSkip = false
+      })
+      io.to(serverCode).emit('lettersSkipped')
+      newLetters()
+    }
+    else {
+      io.to(serverCode).emit('updateVoteSkip', voteSkipCount)
     }
   })
 
